d8235fa3d37c63258a829272e83fdf45a6c7c4fa
cumbinar <cumbiweb@gmail.com>
2022-01-27 18:25:53 -0500

    base web y optimización imágenes

---
+++ gulpfile.js
@@ -0,0 +1,68 @@
+const { src, dest, watch, series, parallel } = require('gulp');
+//CSS SASS
+const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+
+//IMAGENES
+
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
+
+function css( done ) {
+    /*Compilar SASS*/
+    /*Pasos: 1 identificar archivo 2 Compilar 3 guardar el CSS*/
+    src('src/scss/app.scss')
+        .pipe(sass())
+        .pipe(postcss([autoprefixer(dev)]))
+        .pipe(dest('build/css'))
+    done();
+}
+
+
+
+
+function imagenes(done) {
+    src('src/img/**/*')
+        .pipe(imagemin())
+        .pipe(dest('build/img'))
+    done();
+}
+
+
+function versionWebp() {
+    return src('src/img/**/*.{png,jpg}')
+        .pipe(webp())
+        .pipe(dest('build/img'))
+}
+
+function versionAvif() {
+     return src('src/img/**/*.{png,jpg}')
+        .pipe(avif())
+        .pipe(dest('build/img'))
+}
+
+
+function dev() {
+    watch( 'src/scss/**/*.scss', css );
+    watch( 'src/img/**/*', imagenes );
+}
+
+exports.css = css;
+exports.dev = dev;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.default = series(imagenes, versionWebp, versionAvif, css, dev );
+
+// series: Se inicia una tarea, y hasta que finaliza, inicia la siguiente
+
+//parallel: Todas se inician al mismo tiempo
+
+var gulp = require('gulp');
+
+gulp.task('cumbi', function(done) {
+  console.log('Hello Cumbi!!!');
+  done();
+});
